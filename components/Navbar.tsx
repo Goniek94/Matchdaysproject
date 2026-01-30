7160537aab88bac655b8a46f349629b0e5bf897b
Mateusz Goszczycki <mateuszgoszczycki1994@gmail.com>
2026-01-30 12:38:38 +0100

    poprawki

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -501,7 +501,7 @@ export default function Navbar() {
                     setIsMobileMenuOpen(false);
                     handleLoginClick();
                   }}
-                  className="w-full px-6 py-3 bg-white text-black border-2 border-black text-base font-bold rounded-lg hover:bg-black hover:text-white transition-all"    
+                  className="w-full px-6 py-3 bg-white text-black border-2 border-black text-base font-bold rounded-lg hover:bg-black hover:text-white transition-all"
                 >
                   Login
                 </button>
